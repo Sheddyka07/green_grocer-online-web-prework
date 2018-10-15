f91ed74d0daa6e9b03e5c4b03d33a38663ddb4dd
Kelly Sheddy <jungaka07@gmail.com>
2018-10-15 14:04:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -11,6 +11,7 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon| 
-    if cart.has_key? (coupon[:item]) && cart[:item][:count] >= coupon
+    if cart.has_key? (coupon[:item]) && cart[:item][:count] >= coupon[:num]
+      
     
     
